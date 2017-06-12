e6770967ac63b2a294ad29fe9caf1945433108be
Megalawls <daniel.russell@jrweb.org.uk>
2017-06-12 16:52:26 +0100

    Updating files

--- src/main/scala/Person.scala
+++ src/main/scala/Person.scala
@@ -1,4 +1,21 @@
 
-abstract case class Person(fName: String, lName: String) {
+abstract class Person(fName: String, lName: String, contactNo: String)
 
+ case class Employee(fName: String, lName: String, contactNo: String, employeeType: String) extends Person(fName, lName, contactNo) {
+  val id: Int = Employee.nextID()
+
+  override def toString = s"$fName $lName, \nEmployee No:  $id, Employee Type: $employeeType"
+}
+
+object Employee {
+  var employeeId = 0
+
+  def nextID(): Int = {
+    employeeId += 1
+    employeeId
+  }
+}
+
+case class Customer(fName: String, lName: String, contactNo: String, customerNo: Int) extends Person(fName, lName, contactNo) {
+  override def toString = s"$fName $lName, \nCustomer No:  $customerNo, Phone: $customerNo"
 }
---
+++ src/main/scala/Runner.scala
@@ -0,0 +1,9 @@
+
+object Runner extends App{
+  val Employee1 = Employee("Dave", "Davies", "07983238185", "Mechanic")
+  println(Employee1.toString())
+
+  val Employee2 = Employee("Steven", "Stevies", "07983238185", "Mechanic")
+  println(Employee2.toString())
+
+}
