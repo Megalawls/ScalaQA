c7b68b30d575bdcd61b5d2b2d7dd3cdba25d7ad6
Megalawls <daniel.russell@jrweb.org.uk>
2017-06-12 15:09:16 +0100

    Updating files

--- src/main/scala/BusDriver.sc
+++
@@ -1,2 +0,0 @@
-var busRoute: List[String]
-
--- src/main/scala/VehicleWS.scala
+++ src/main/scala/VehicleWS.scala
@@ -2,15 +2,8 @@ import scala.collection.mutable.ArrayBuffer
 
 abstract class Vehicle(vehicleType: String, make: String, model: String, reg: String){
   case class Part(name: String, isBroken: Boolean, costToFix: Double, quantity: Int) // Create a full parts list with boolean car/bike
-  var vehicleParts = new ArrayBuffer[Part]
-
-  override def toString = s"$vehicleType - $make $model - Reg:$reg \nParts: $vehicleParts"
-
-  def populateParts(wheels: Int): Unit = {
-
-  }
+  var carParts = new ArrayBuffer[Part]
 }
 
 case class Car(vehicleType: String, make: String, model: String, reg: String) extends Vehicle(vehicleType = "Car", make, model, reg)
 
-case class Motorbike(vehicleType: String, make: String, model: String, reg: String) extends Vehicle(vehicleType = "Motorbike", make, model, reg)
